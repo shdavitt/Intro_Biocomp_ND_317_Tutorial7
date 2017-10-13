21dafd5cecbe9545723a6cb8161f8c7dbb027238
shdavitt <shdavitt@gmail.com>
2017-10-12 20:42:17 -0400

    Question 1, plotting sequences by lengths.

--- tut7RScript.R
+++ tut7RScript.R
@@ -2,3 +2,20 @@
 #Shane Davitt (No partner as I was the odd one out in our tutorial section)
 #
 #Question 1
+#
+#{Plotting by Sequence Lengths}
+#
+#Putting Lecture11.fasta into an R object
+fastaData=read.table("Lecture11.fasta", sep="\t")
+#
+#Removing seq labels
+conFasta = fastaData[seq(2, nrow(fastaData),2),]
+#
+#Converting data to a dataframe
+data.frame(conFasta)
+#
+#Counting number of charecters in each sequence
+countFasta=apply(conFasta,1,nchar)
+#
+#Plotting the result as a histogram
+seqLenPlot=hist(countFasta, xlab="Sequence Length")
